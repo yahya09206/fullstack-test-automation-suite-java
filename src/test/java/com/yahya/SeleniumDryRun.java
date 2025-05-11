b2aeafb04ed56ecfd0b236a5c4d98529c470c652
Yahya <yahyahussein91@gmail.com>
2025-05-11 01:52:39 -0700

    set up project locally

---
+++ src/test/java/com/yahya/SeleniumDryRun.java
@@ -0,0 +1,19 @@
+package com.yahya;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDryRun {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+        Thread.sleep(2000);
+
+        driver.quit();
+
+    }
+}
