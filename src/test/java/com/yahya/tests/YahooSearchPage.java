b67f3bcf8750b7fe684d641bfeaa92c1e3259266
Yahya <yahyahussein91@gmail.com>
2025-05-17 03:09:06 -0700

    set up for yahoo search

--- src/test/java/com/yahya/tests/YahooSearchPage.java
+++ src/test/java/com/yahya/tests/YahooSearchPage.java
@@ -1,8 +1,12 @@
 package com.yahya.tests;
 
+import com.yahya.utility.Driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class YahooSearchPage {
 
@@ -12,4 +16,16 @@ public class YahooSearchPage {
     public static void setupDriver(){
         WebDriverManager.chromedriver().setup();
     }
+
+    @BeforeEach
+    public void setupWebDriver(){
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void closeBrowser(){
+        driver.quit();
+    }
+
+
 }
