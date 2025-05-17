a2b720ad857fb5555e2e2c54ed1d0770d607fa15
Yahya <yahyahussein91@gmail.com>
2025-05-17 03:28:30 -0700

    getting attribute of checked value

--- src/test/java/com/yahya/day04/LocatingCheckboxes.java
+++ src/test/java/com/yahya/day04/LocatingCheckboxes.java
@@ -15,6 +15,7 @@ public class LocatingCheckboxes {
          * Identify first check box
          *  check if box1 is initially selected
          *  check if box2 is selected
+         *  get value of "checked" attribute
          */
 
         WebDriverManager.chromedriver().setup();
@@ -23,9 +24,11 @@ public class LocatingCheckboxes {
 
         WebElement firstBox = driver.findElement(By.id("box1"));
         System.out.println("firstBox.isSelected() = " + firstBox.isSelected());
+        System.out.println("firstBox.getAttribute(\"checked\") = " + firstBox.getAttribute("checked"));
 
         WebElement secondBox = driver.findElement(By.id("box2"));
         System.out.println("secondBox.isSelected() = " + secondBox.isSelected());
+        System.out.println("secondBox.getAttribute(\"checked\") = " + secondBox.getAttribute("checked"));
 
         driver.quit();
 
