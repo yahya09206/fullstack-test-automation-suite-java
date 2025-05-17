ab5b43406df9b927d56958fe1220dcfa4dbdf090
Yahya <yahyahussein91@gmail.com>
2025-05-17 04:48:18 -0700

    selecting radio buttons

--- src/test/java/com/yahya/day04/LocatingCheckboxes.java
+++ src/test/java/com/yahya/day04/LocatingCheckboxes.java
@@ -16,19 +16,37 @@ public class LocatingCheckboxes {
          *  check if box1 is initially selected
          *  check if box2 is selected
          *  get value of "checked" attribute
+         *      click 1st box to check it
+         *      click 2nd box to uncheck it
+         *          check if either are already selected by using if statement
          */
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://practice.cydeo.com/checkboxes");
 
-        WebElement firstBox = driver.findElement(By.id("box1"));
-        System.out.println("firstBox.isSelected() = " + firstBox.isSelected());
-        System.out.println("firstBox.getAttribute(\"checked\") = " + firstBox.getAttribute("checked"));
+        WebElement checkbox1 = driver.findElement(By.id("box1"));
+        System.out.println("checkbox1.isSelected() = " + checkbox1.isSelected());
+        System.out.println("checkbox1.getAttribute(\"checked\") = " + checkbox1.getAttribute("checked"));
 
-        WebElement secondBox = driver.findElement(By.id("box2"));
-        System.out.println("secondBox.isSelected() = " + secondBox.isSelected());
-        System.out.println("secondBox.getAttribute(\"checked\") = " + secondBox.getAttribute("checked"));
+        WebElement checkbox2 = driver.findElement(By.id("box2"));
+        System.out.println("checkbox2.isSelected() = " + checkbox2.isSelected());
+        System.out.println("checkbox2.getAttribute(\"checked\") = " + checkbox2.getAttribute("checked"));
+
+        checkbox1.click();
+        checkbox2.click();
+
+        if (!checkbox1.isSelected()){
+            checkbox1.click();
+        } else {
+            System.out.println("Already selected");
+        }
+
+        if (!checkbox2.isSelected()){
+            checkbox2.click();
+        } else {
+            System.out.println("Already selected");
+        }
 
         driver.quit();
 
---
+++ src/test/java/com/yahya/day04/LocatingRadioButtons.java
@@ -0,0 +1,14 @@
+package com.yahya.day04;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LocatingRadioButtons {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://practice.cydeo.com/radio_buttons");
+    }
+}
