80184a2e3cf75fce9940208c98a9da3c2edf3322
Yahya <yahyahussein91@gmail.com>
2025-05-18 01:25:59 -0700

    elements by css selector

---
+++ src/test/java/com/yahya/day04/FindElementByCssSelector.java
@@ -0,0 +1,29 @@
+package com.yahya.day04;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementByCssSelector {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://search.yahoo.com/");
+
+        // by id
+        // WebElement searchBox = driver.findElement(By.cssSelector("#yschsp"));
+        // by name
+        // WebElement searchBox = driver.findElement(By.cssSelector("input[name='p']"));
+        // multiple
+        WebElement searchBox = driver.findElement(By.cssSelector("input[name='p'][type='text']"));
+        searchBox.sendKeys("CSS Selector");
+
+        Thread.sleep(2000);
+
+        driver.quit();
+
+    }
+}
