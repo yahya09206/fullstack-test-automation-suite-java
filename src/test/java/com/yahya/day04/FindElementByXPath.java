08f16c59408223920d26c947dd88cff4a814d5ad
Yahya <yahyahussein91@gmail.com>
2025-05-18 01:45:13 -0700

    getting text of header using xpath

--- src/test/java/com/yahya/day04/FindElementByXPath.java
+++ src/test/java/com/yahya/day04/FindElementByXPath.java
@@ -1,8 +1,19 @@
 package com.yahya.day04;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FindElementByXPath {
     public static void main(String[] args) {
 
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://practice.cydeo.com/");
 
+        WebElement header1 = driver.findElement(By.xpath("//h1/span[@class='h1y']"));
+        System.out.println("header1.getText() = " + header1.getText());
     }
 }
