e78b19cd24e7db58a00808450c4a1af86b13e9cb
yahya09206 <yahyahussein91@gmail.com>
2025-05-23 16:23:24 -0700

    file upload script complete

--- src/test/java/com/yahya/tests/day08/FileUpload.java
+++ src/test/java/com/yahya/tests/day08/FileUpload.java
@@ -2,12 +2,22 @@ package com.yahya.tests.day08;
 
 import com.yahya.utility.TestBase;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class FileUpload extends TestBase {
 
     @Test
-    public void testUpload(){
+    public void testUpload() throws InterruptedException {
 
         driver.get("https://practice.cydeo.com/upload");
+
+        WebElement inputField = driver.findElement(By.id("file-upload"));
+        String filePath = "/Users/yahyahussein91/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Screenshot 2025-04-01 at 12.43.36 PM.png";
+        inputField.sendKeys(filePath);
+
+        driver.findElement(By.id("file-submit")).click();
+
+        Thread.sleep(2000);
     }
 }
