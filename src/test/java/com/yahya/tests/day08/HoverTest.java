0f5435bce0f430d962e4c877c1c9cee4c5cb98d1
yahya09206 <yahyahussein91@gmail.com>
2025-05-23 17:17:06 -0700

    chained performance together

--- src/test/java/com/yahya/tests/day08/HoverTest.java
+++ src/test/java/com/yahya/tests/day08/HoverTest.java
@@ -2,6 +2,7 @@ package com.yahya.tests.day08;
 
 import com.yahya.utility.BrowserUtil;
 import com.yahya.utility.TestBase;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -20,6 +21,23 @@ public class HoverTest extends TestBase {
         Actions actions = new Actions(driver);
         actions.moveToElement(firstImg).perform();
 
+        WebElement firstProfileElm = driver.findElement(By.xpath("//div/h5[.='name: user1']"));
+        Assertions.assertTrue(firstProfileElm.isDisplayed());
+
         BrowserUtil.waitFor(2);
+
+        WebElement secondImg = driver.findElement(By.xpath("(//div[@class='figure']/img)[2]"));
+        actions.moveToElement(secondImg).perform();
+        BrowserUtil.waitFor(2);
+
+        WebElement thirdImg = driver.findElement(By.xpath("(//div[@class='figure']/img)[3]"));
+        actions.moveToElement(thirdImg).perform();
+        BrowserUtil.waitFor(2);
+
+        actions.moveToElement(firstImg).pause(2000)
+                .moveToElement(secondImg).pause(2000)
+                .moveToElement(thirdImg).pause(2000).perform();
+
+        BrowserUtil.waitFor(2000);
     }
 }
