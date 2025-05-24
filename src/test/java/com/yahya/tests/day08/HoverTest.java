ff1ba70eda2a9432a5c8650755ef61d19ed5cb71
yahya09206 <yahyahussein91@gmail.com>
2025-05-23 17:25:02 -0700

    workflow for perfoming keyboard actions

--- src/test/java/com/yahya/tests/day08/HoverTest.java
+++ src/test/java/com/yahya/tests/day08/HoverTest.java
@@ -5,6 +5,7 @@ import com.yahya.utility.TestBase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
@@ -39,5 +40,30 @@ public class HoverTest extends TestBase {
                 .moveToElement(thirdImg).pause(2000).perform();
 
         BrowserUtil.waitFor(2000);
+    }
+
+    @Test
+    public void testKeyboardAction(){
+
+        // Navigate to google.com
+        // hold down to shift enter text "I love selenium"
+        // release the shift
+        // enter the text "i love selenium"
+        // hold down the command on mac control
+        // hit backspace to delete
+        driver.get("https://www.google.com");
+        // locate search box using name value q
+        WebElement searchBox = driver.findElement(By.name("q"));
+        // create actions class instance
+        Actions actions = new Actions(driver);
+        // keyDown method for holding down certain modifiers like control, shift and so on
+        // keyUp method for releasing what you are holding down
+        // sendKeys method of Actions class is for pressing key that is provided
+        // pause method is for pausing in between actions in ms
+        actions.keyDown(Keys.SHIFT).sendKeys("i love selenium")
+                .pause(2000).keyUp(Keys.SHIFT)
+                .sendKeys("i love selenium").pause(2000)
+                .perform();
+
     }
 }
