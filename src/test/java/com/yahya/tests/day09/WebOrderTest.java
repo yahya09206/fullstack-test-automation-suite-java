bf6936cec58110a8efe66fa3b99e941bd805ed24
Yahya <yahyahussein91@gmail.com>
2025-05-24 01:40:29 -0700

    assert that header is displayed

--- src/test/java/com/yahya/tests/day09/WebOrderTest.java
+++ src/test/java/com/yahya/tests/day09/WebOrderTest.java
@@ -1,9 +1,13 @@
 package com.yahya.tests.day09;
 
+import com.yahya.utility.BrowserUtil;
 import com.yahya.utility.TestBase;
 import com.yahya.utility.WebOrderUtility;
+import io.restassured.internal.common.assertion.Assertion;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class WebOrderTest extends TestBase {
 
@@ -14,6 +18,12 @@ public class WebOrderTest extends TestBase {
 
 //        WebOrderUtility.login(driver);
         WebOrderUtility.login(driver, "Tester", "test");
+
+        WebElement h1Header = driver.findElement(By.xpath("//div/h2[normalize-space()='List of All Orders']"));
+        Assertions.assertTrue(h1Header.isDisplayed());
+
+        BrowserUtil.waitFor(3);
+        WebOrderUtility.logout(driver);
 
     }
 }
