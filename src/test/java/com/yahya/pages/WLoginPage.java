6780fea1a0207a7814e7f3fe80f3c6db9c4d60f5
Yahya <yahyahussein91@gmail.com>
2025-05-25 01:07:18 -0700

    set up page for logging into weborder app

--- src/test/java/com/yahya/pages/WLoginPage.java
+++ src/test/java/com/yahya/pages/WLoginPage.java
@@ -1,10 +1,23 @@
 package com.yahya.pages;
 
+import com.yahya.utility.Driver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class WLoginPage {
 
+    @FindBy(id = "ctl00_MainContent_username")
     private WebElement usernameField;
+    @FindBy(id = "ctl00_MainContent_password")
     private WebElement passwordField;
+    @FindBy(xpath = "//div/input[@name='ctl00$MainContent$login_button']")
     private WebElement loginButton;
+
+    // No arg constructor to instruct selenium
+    public WLoginPage(){
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
 }
---
+++ src/test/java/com/yahya/tests/day11/WebOrderPOMTest.java
@@ -0,0 +1,13 @@
+package com.yahya.tests.day11;
+
+import com.yahya.utility.TestBase;
+import org.junit.jupiter.api.Test;
+
+public class WebOrderPOMTest extends TestBase {
+
+    @Test
+    public void testWithPOMForLogin(){
+
+
+    }
+}
