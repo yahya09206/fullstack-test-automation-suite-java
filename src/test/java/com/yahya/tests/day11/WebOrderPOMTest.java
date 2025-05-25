4fb0d7c763b45fa643b75f4e777370181e5fe35d
Yahya <yahyahussein91@gmail.com>
2025-05-25 04:00:48 -0700

    set up cucumber and first feature file

--- src/test/java/com/yahya/tests/day11/WebOrderPOMTest.java
+++ src/test/java/com/yahya/tests/day11/WebOrderPOMTest.java
@@ -1,10 +1,12 @@
 package com.yahya.tests.day11;
 
 import com.yahya.pages.WAllOrderPage;
+import com.yahya.pages.WCommonArea;
 import com.yahya.pages.WLoginPage;
 import com.yahya.utility.BrowserUtil;
 import com.yahya.utility.Driver;
 import com.yahya.utility.TestBase;
+import com.yahya.utility.WebOrderUtil;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -40,6 +42,25 @@ public class WebOrderPOMTest extends TestBase {
 
         allOrderPage.uncheckAllBtn.click();
 
+        BrowserUtil.waitFor(2);
+    }
+
+    @Test
+    public void testCommonArea(){
+
+        WLoginPage loginPage = new WLoginPage();
+
+        loginPage.goTo();
+        loginPage.login("Tester", "test");
+
+        WCommonArea commonArea = new WCommonArea();
+        commonArea.orderTab.click();
+        BrowserUtil.waitFor(2);
+
+        commonArea.viewAllOrderTab.click();
+        BrowserUtil.waitFor(2);
+
+        commonArea.viewAllProductTab.click();
         BrowserUtil.waitFor(2);
     }
 }
