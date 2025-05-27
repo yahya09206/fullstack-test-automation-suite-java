eec9ed369bbe163642c24ce5c04953b5d4701bd2
yahya09206 <yahyahussein91@gmail.com>
2025-05-26 18:13:09 -0700

    removed some files and fixed hooks class

--- src/test/java/com/yahya/step_definitions/GoogleSearchStepDef.java
+++ src/test/java/com/yahya/step_definitions/GoogleSearchStepDef.java
@@ -1,16 +1,13 @@
 package com.yahya.step_definitions;
 
-import com.yahya.pages.GoogleHomePage;
-import io.cucumber.java.en.Given;
-
 public class GoogleSearchStepDef {
 
-    GoogleHomePage homePage;
-
-    @Given("user is at the homepage")
-    public void user_is_at_the_homepage() {
-
-        homePage = new GoogleHomePage();
-        homePage.goTo();
-    }
+//    GoogleHomePage homePage;
+//
+//    @Given("user is at the homepage")
+//    public void user_is_at_the_homepage() {
+//
+//        homePage = new GoogleHomePage();
+//        homePage.goTo();
+//    }
 }
