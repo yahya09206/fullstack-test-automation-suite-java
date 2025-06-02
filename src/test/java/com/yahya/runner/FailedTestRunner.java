ff40db87656d101a043f330f15eff84f899ac210
yahya09206 <yahyahussein91@gmail.com>
2025-06-01 22:43:00 -0700

    added clarifying comment

--- src/test/java/com/yahya/runner/FailedTestRunner.java
+++ src/test/java/com/yahya/runner/FailedTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-
+//File for failed tests
 @RunWith(Cucumber.class)
 @CucumberOptions(
         glue = "com/yahya/step_definitions",
