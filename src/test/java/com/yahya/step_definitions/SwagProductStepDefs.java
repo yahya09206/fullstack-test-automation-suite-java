e23970407d08b8c7a683750f80c096b21b21f81e
yahya09206 <yahyahussein91@gmail.com>
2025-06-23 20:53:40 -0700

    setting up step defs class

--- src/test/java/com/yahya/step_definitions/SwagProductStepDefs.java
+++ src/test/java/com/yahya/step_definitions/SwagProductStepDefs.java
@@ -1,4 +1,25 @@
 package com.yahya.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class SwagProductStepDefs {
+
+
+    @Given("I'm at the products page")
+    public void i_m_at_the_products_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I view all of the items")
+    public void i_view_all_of_the_items() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I should see {string} items")
+    public void i_should_see_items(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
